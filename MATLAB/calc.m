c8b80a212e569043de7ab64053246f513e70d22e
mihsha <mzlobins@yandex.ru>
2025-04-21 08:51:17 +0300

    l3

--- MATLAB/calc.m
+++ MATLAB/calc.m
@@ -14,7 +14,7 @@ syms s;
 frac = 5.52/(s^2 + 2.2*s + 4.4);
 syms t;
 u1(t) = 3*heaviside(t) - 6*heaviside(t-1) + 3*heaviside(t-2);
-fplot(imp, [0, 3]); grid on; 
+fplot(u1, [0, 3]); grid on; 
 xlabel("t"); ylabel("U");
 imp_im  = laplace(u1(t))
 u2(t) = vpa(ilaplace(frac*imp_image));
@@ -43,23 +43,41 @@ figure;
 fplot(h(t)); grid on; 
 xlabel("t"); 
 ylabel("h(t)")
-%% АЧХ Ф
+    %% АЧХ Ф
 syms t;
 syms S;
 u1(t) = 3*heaviside(t) - 6*heaviside(t-1) + 3*heaviside(t-2);
 U1(S) = laplace(u1(t));
 syms w;
 A(w) = 6*(sin(1/2*w))^2/w;
-F(w) = angle(-6*exp(-1i*w)*(sin(1/2*w)^2));
+F(w) = angle(6*1i*exp(-1i*w)*(sin(1/2*w)^2));
 figure
 subplot(2,1,1)
 fplot(A(w), [0 25]); xlabel("A(w)"); ylabel("w"); grid on;
 subplot(2,1,2)
 fplot(rad2deg(F(w)),([0 25])); xlabel("w"); ylabel("\Phi(w)"); grid on;
-%% pulsetran
-syms t;
-u1(t) = 3*heaviside(t) - 6*heaviside(t-1) + 3*heaviside(t-2);
-figure
-fplot(u1(mod(t, 4)), [0 20]); grid on; ylabel("u_1(t)"); xlabel("t")
+W = 0.1:0.1:15;
+A2 = A(W); F2 = F(W);
+figure;
+polarplot(A2, F2);
+%% Period
+T = 4;
+syms w;
+A(w) = 6*(sin(1/2*w))^2/w;
+w0 = 2*pi/T;
+W = w0:w0:10*w0;
+A2 = A(W);
+figure;
+stem(W, A2);
 
-u11 = [0 ]
+F(w) = angle(6*1i*exp(-1i*w)*(sin(1/2*w)^2));
+F2 = F(W);
+figure;
+stem(W, rad2deg(F2));
+
+syms t;
+c(t) = cos(W*t + F2);
+h(t) = A2.*c(t);
+u1(t) = sum(h(t));
+figure;
+fplot(t, u1(t), [0 10])
